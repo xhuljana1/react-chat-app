647b17230b896c96f4f7d075e452c250968b758f
xhuljana1 <xhuljana_muskaj@yahoo.com>
2019-05-20 22:51:58 +0200

    Updates

---
+++ precache-manifest.9778d52fb9921e35db3b9c3aa7669e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9c7563e0f222ec244706c9400f31bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "349e4c99fb1f148945dc",
+    "url": "/static/css/main.1a8b6acc.chunk.css"
+  },
+  {
+    "revision": "88badc512418cc698bed",
+    "url": "/static/js/2.0125efae.chunk.js"
+  },
+  {
+    "revision": "349e4c99fb1f148945dc",
+    "url": "/static/js/main.b7ba3567.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
