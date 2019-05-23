78a7d5c659ac6ae2e25d14b17bc732b31c3f861a
xhuljana1 <xhuljana_muskaj@yahoo.com>
2019-05-23 23:36:53 +0200

    Updates

---
+++ precache-manifest.3c8f80b00aae3bf10b4e00d027c5e3bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-chat-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e2a219927225f2f3d518",
+    "url": "/react-chat-app/static/js/runtime~main.e2a21992.js"
+  },
+  {
+    "revision": "eb11084cf0eeb4e6988e",
+    "url": "/react-chat-app/static/js/main.eb11084c.chunk.js"
+  },
+  {
+    "revision": "c78c1ad13581c7146757",
+    "url": "/react-chat-app/static/js/1.c78c1ad1.chunk.js"
+  },
+  {
+    "revision": "eb11084cf0eeb4e6988e",
+    "url": "/react-chat-app/static/css/main.a31c43d3.chunk.css"
+  },
+  {
+    "revision": "f25849dcbe5c7e06da577d30363c23be",
+    "url": "/react-chat-app/index.html"
+  }
+];
